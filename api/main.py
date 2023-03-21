fae17d79d4a55c573978c4d1162056a066acf3dd
shachar-karry <shachar@karry.ai>
2023-03-21 15:10:57 +0200

    add 2 endpoints

--- api/main.py
+++ api/main.py
@@ -8,4 +8,12 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/r1")
+async def root():
+    return {"message": "r1"}
+
+@app.get("/r2")
+async def root():
+    return {"message": "r2"}
+
 handler = Mangum(app=app)
