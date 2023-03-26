163e98c2e59545369fa4eca8cb8f437751e983b9
shachar-karry <shachar@karry.ai>
2023-03-26 15:36:23 +0300

    Add gitlog endpoint

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
 from mangum import Mangum
 from fastapi import FastAPI
+import subprocess
 
+#app = FastAPI(root_path="/dev/")
 app = FastAPI()
 
 
@@ -8,9 +10,16 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+
+@app.get("/gitlog")
+async def root():
+    return {"message": subprocess.check_output(["git", "log", "-n", "1"]) }
+
+
 @app.get("/r1")
 async def root():
     return {"message": "r1"}
+
 
 @app.get("/r2")
 async def root():
