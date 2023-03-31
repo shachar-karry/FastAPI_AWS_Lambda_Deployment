9f9d4ffabdc871816184731764227d614108b8d2
shachar-karry <shachar@karry.ai>
2023-03-31 15:33:20 +0300

    shabatbot

--- SNS_Play.py
+++ SNS_Play.py
@@ -2,10 +2,31 @@ from dotenv import load_dotenv, find_dotenv
 import os
 import boto3
 
+phone_book = \
+{
+    "shachar":"+972523370403",
+    "yochay":"+972523798835",
+    "gilad": "+972547824487"
+}
+
 load_dotenv(find_dotenv())
 
 sns_client = boto3.client("sns")
 
-for i in range(3):
-    sns_client.publish(PhoneNumber="+972523370403", Message=f"Hello SNS ({i})")
+#sns_client.publish(PhoneNumber="+972523370403", Message="Hello SNS")
+
+shabat_str = \
+"מזמור שיר ליום השבת, טוב להודות לה' ולזמר לשמך עליון. " \
+"בכבוד השבת, נכנס לאוירה רוחנית ומפנקת של נוחות ומנוחה, ומתפנקים בקדושה ובשמחה. " \
+"שבת שלום מהשבתבוט"
+
+for id in phone_book:
+    print("sending to :", id, phone_book[id])
+    response = sns_client.publish(PhoneNumber=phone_book[id], Message=shabat_str)
+
+
+#response = sns_client.publish(PhoneNumber="+972523370403", Message="בעזרת השם, הסמס הזה יגיע אליכם בשעה טובה וימלא את ביתכם בקדושתה של השבת המבורכת הקרובה. אמן.")
+
+#response
+
 
