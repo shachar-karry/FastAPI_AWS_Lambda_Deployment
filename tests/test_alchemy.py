99e6c7e050c39a3fe5ed2f18b82e14e89a0e7681
shachar-karry <shachar@karry.ai>
2023-03-31 20:14:10 +0300

    MY SQL test

--- tests/test_alchemy.py
+++ tests/test_alchemy.py
@@ -1,10 +1,23 @@
 from fastapi.testclient import TestClient
 from api.alchemy_api1 import app
 
-client = TestClient(app)
+import sqlalchemy
+from dotenv import load_dotenv, find_dotenv
+
+import os
+
+
+def test_mysql_db():
+    load_dotenv(find_dotenv())
+    # create a SQLAlchemy engine and sessionmaker
+    conn_str = os.getenv("MYSQL_CONNECTION_STR")
+    engine = sqlalchemy.create_engine(conn_str, echo=True)  # connect to server
+    print()
+    print(engine)
 
 
 def test_health():
+    client = TestClient(app)
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"message": "healthy"}
